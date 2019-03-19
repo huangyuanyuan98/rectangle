cad2818f1a61ac8c8597a639bdcef5a66772fba2
wangding <408542507@qq.com>
2019-03-19 12:33:45 +0800

    v0.5

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
       options: {
         configFile: '.eslintrc.json'
       },
-      target: ['rectangle.js']        
+      target: ['*.js']        
     },
    
     csslint: {
@@ -51,7 +51,7 @@ module.exports = function (grunt) {
           preserveLineBreaks: false                                              
       },     
       files: {    
-          src: './index.html',             
+          src: 'dist/index.html',             
           dest: 'dist/index.html'                                               
       }            
     },
@@ -63,10 +63,38 @@ module.exports = function (grunt) {
     uglify: {       
       release:{        
         files: {
-          'dist/rectangle.js': 'rectangle.js',                   
-          'dist/calc.js': 'calc.js'                                                                        
+          'dist/bundle.min.js': 'dist/bundle.js',          
         }                                     
       }
+    },
+
+    useminPrepare: {     
+      html: 'index.html',         
+        options: {           
+          dest: 'dist'                                           
+        }                       
+    },
+     
+    usemin: {         
+      html: ['dist/index.html']                                
+    },
+    
+    concat: {       
+      options: {        
+        separator: ';'
+      },      
+      js: {       
+        src: ['rectangle.js', 'calc.js'],                
+        dest: ['dist/bundle.js']                                                        
+      }                 
+    },     
+        
+    clean: ['dist/bundle.js', '.tmp'],        
+    
+    copy: {           
+      html: {            
+        src: './index.html',                   
+        dest: './dist/index.html'                                                            }
     }
   });
 
@@ -79,10 +107,14 @@ grunt.loadNpmTasks('grunt-mocha');
 grunt.loadNpmTasks('grunt-contrib-htmlmin');  
 grunt.loadNpmTasks('grunt-contrib-cssmin');   
 grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-concat'); 
+grunt.loadNpmTasks('grunt-contrib-copy');  
+grunt.loadNpmTasks('grunt-usemin');     
+grunt.loadNpmTasks('grunt-contrib-clean');
 
 grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
 grunt.registerTask('cover', ['mocha_istanbul']);
 grunt.registerTask('check-cover', ['istanbul_check_coverage']);
 grunt.registerTask('unitTest', ['mocha']);
-grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
+grunt.registerTask('release', ['copy', 'useminPrepare', 'concat', 'uglify', 'usemin', 'cssmin', 'htmlmin', 'clean']);
 };
