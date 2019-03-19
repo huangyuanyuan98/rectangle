620bd35308eae32c991e525913574887e6224a3a
wangding <408542507@qq.com>
2019-03-19 09:30:25 +0800

    矩形计算器 v0.4

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,6 +44,30 @@ module.exports = function (grunt) {
           reporter: 'Spec'                       
       }    
     },
+
+    htmlmin: {    
+      options: {    
+          collapseWhitespace: true,            
+          preserveLineBreaks: false                                              
+      },     
+      files: {    
+          src: './index.html',             
+          dest: 'dist/index.html'                                               
+      }            
+    },
+     
+    cssmin: {   
+      'dist/rectangle.css': 'rectangle.css'                         
+    },
+     
+    uglify: {       
+      release:{        
+        files: {
+          'dist/rectangle.js': 'rectangle.js',                   
+          'dist/calc.js': 'calc.js'                                                                        
+        }                                     
+      }
+    }
   });
 
 grunt.loadNpmTasks('grunt-contrib-csslint');
@@ -52,9 +76,13 @@ grunt.loadNpmTasks('grunt-eslint');
 grunt.loadNpmTasks('grunt-mocha-cli');
 grunt.loadNpmTasks('grunt-mocha-istanbul');
 grunt.loadNpmTasks('grunt-mocha');
+grunt.loadNpmTasks('grunt-contrib-htmlmin');  
+grunt.loadNpmTasks('grunt-contrib-cssmin');   
+grunt.loadNpmTasks('grunt-contrib-uglify');
 
 grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
 grunt.registerTask('cover', ['mocha_istanbul']);
 grunt.registerTask('check-cover', ['istanbul_check_coverage']);
 grunt.registerTask('unitTest', ['mocha']);
+grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
 };
