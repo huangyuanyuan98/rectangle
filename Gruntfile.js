3baf4db9414460bf17db9fdf8987e79fac322fe1
wangding <408542507@qq.com>
2019-03-19 14:33:12 +0800

    v0.5

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,11 +63,8 @@ module.exports = function(grunt) {
     uglify: {
       release:{
         files: {
-          /*'dist/rectangle.js': 'rectangle.js',
-          'dist/calc.js': 'calc.js'*/
-          'dist/bundle.min.js': 'dist/bundle.js',                                
-        }
-          
+          'dist/bundle.min.js': 'dist/bundle.js',                   
+        } 
       }
     },
     useminPrepare: {
