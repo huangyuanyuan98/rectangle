f8eb220ebebbc3e471573f90a7ddf65d344a9a22
wangding <408542507@qq.com>
2019-03-19 08:15:59 +0800

    矩形计算器 v0.3

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,30 +2,59 @@ module.exports = function (grunt) {
   grunt.initConfig({
     eslint: {
       options: {
-                configFile: '.eslintrc.json'
-          
+        configFile: '.eslintrc.json'
       },
-        target: ['rectangle.js']
-        
+      target: ['rectangle.js']        
     },
-  csslint: {
-    options: {
-              csslintrc: '.csslintrc'                    
+   
+    csslint: {
+      options: {
+        csslintrc: '.csslintrc'                    
+      },
+      src: '*.css'             
     },
-          src: '*.css'
-                
-  },
-  htmlhint: {
-    options: {
-              htmlhintrc: '.htmlhintrc'                  
+
+    htmlhint: {
+      options: {
+        htmlhintrc: '.htmlhintrc'                  
+      },
+      src: '*.html'               
     },
-          src: '*.html'               
-    }
+
+    mochacli: {
+      options: {
+        reporter: 'spec',
+        bail: true                              
+      },
+      all: ['test/*.js'] 
+    },
+  
+    mocha_istanbul: {
+      coverage: {
+        src: 'test'                
+      }      
+    },
+
+    mocha: {
+      test: {
+          src: ['test/index.html']               
+      },
+      options: {
+          run: true,
+          reporter: 'Spec'                       
+      }    
+    },
   });
 
 grunt.loadNpmTasks('grunt-contrib-csslint');
 grunt.loadNpmTasks('grunt-htmlhint');
 grunt.loadNpmTasks('grunt-eslint');
+grunt.loadNpmTasks('grunt-mocha-cli');
+grunt.loadNpmTasks('grunt-mocha-istanbul');
+grunt.loadNpmTasks('grunt-mocha');
 
 grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
+grunt.registerTask('cover', ['mocha_istanbul']);
+grunt.registerTask('check-cover', ['istanbul_check_coverage']);
+grunt.registerTask('unitTest', ['mocha']);
 };
--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,4 @@
+/* global Rectangle: true */
 $(function(){
   //console.log("hello");
   var $width   = $('#width'),
@@ -9,10 +10,9 @@ $btnCal.click(function(){
   var w = Number($width.val()),
       h = Number($height.val());
   
-  var p = 2 * (w + h),
-      a = w * h;
+  var r = new Rectangle(w,h);
   
-  $perimeter.val(p);
-  $area.val(a);
+  $perimeter.val(r.$perimeter());
+  $area.val(r.area());
 })
 });
