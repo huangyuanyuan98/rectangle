da8a2fba6b78614cace22cb0ff64e57440a41fbc
huangyuanyuan98 <930317972@qq.com>
2019-04-02 14:55:50 +0800

    字符校验

--- rectangle.js
+++ rectangle.js
@@ -5,6 +5,26 @@ $(function(){
       $btnCal  = $('#calculate'),
       $perimeter = $('#perimeter'),
       $area    = $('#area');
+$width.keypress(function(e){
+  if(/[abcdf-zABCDF-Z`~!@#$%^&*()=_+[\]{}|;:'",<>/?\\]"']`]/.test(e.key)){
+    e.preventDefault();
+  }
+  if(e.key === '.'){
+    if(e.target.value === '') e.preventDefault();
+    if(e.target.value.indexOf('.' !== -1)){
+
+    }
+  }
+})
+
+$width.focusout(function(){
+  var result = valid($width.val());
+
+  if(!result.isOK){
+    $width
+  }
+})
+
 $btnCal.click(function(){
   var w = Number($width.val()),
       h = Number($height.val());
